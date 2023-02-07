f4ef62abf2c508902ff7726b76278ec5d29034e3
Karthick Raj K <karthickraj2001@gmail.com>
2023-02-07 23:34:20 +0530

    Commiting

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
+
 public class Main {
-  
+
+    public static class A{
+        String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        @Override
+        String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String[] args)
+    {
+        A b = new B();
+        b.meth();
+
+
+        B bb = new B();
+        bb.meth();
+    }
 }
